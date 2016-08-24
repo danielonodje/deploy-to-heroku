2359464be271f86063531e911ff432403bb5e910
reddberckley <danielonodje@gmail.com>
2016-08-24 12:56:32 +0100

    another

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+app.set('port' || (process.env.PORT || 8800));
 
 app.get('/',function(request,response){
     response.send("I am become Death. Destroyer of worlds");
 });
 
-app.listen(8800);
+app.listen(app.get('port'),function(){
+    console.log('Node app is running on port', app.get('port'));
+});
